fa04c0938cfea0b9562af30c8cc0b38a05fcdc39
Louis Diane <louisdiane24@gmail.com>
2015-04-08 11:11:24 +0200

    fix encoding

--- server-install.sh
+++ server-install.sh
@@ -1,4 +1,3 @@
-set -o errexit #abort if any command fails
 echo "############## INSTALL GIT ####################"
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update
@@ -21,12 +20,10 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install mongodb-10gen
-set +o errexit #don't abort if any command fails
 mkdir /data/
 mkdir /data/db
 chmod 777 /data/db
 mongod --version
-set -o errexit #abort if any command fails
 cd /media/sf_tp-p2018
 npm install --no-bin-links
 echo '############## INSTALL SERVER APP ####################'

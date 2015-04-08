83a2462d9c0af08d645364c9f508febd9487bd81
Louis Diane <louisdiane24@gmail.com>
2015-04-08 11:05:24 +0200

    fix encoding

--- server-install.sh
+++ server-install.sh
@@ -1,41 +1,38 @@
 set -o errexit #abort if any command fails
-echo '############## INSTALL GIT ####################'
-add-apt-repository -y ppa:git-core/ppa
-apt-get update
-apt-get install -y git
-apt-get install -y git-core bash-completion
-echo $(git –-version)
+echo "############## INSTALL GIT ####################"
+sudo add-apt-repository -y ppa:git-core/ppa;
+sudo apt-get update;
+sudo apt-get install -y git;
+sudo apt-get install -y git-core bash-completion;
 echo '############## INSTALL NODE ####################'
-apt-get update
-apt-get install -y python-software-properties python g++ make
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get update
-apt-get install -y nodejs
-node --version
+sudo apt-get update;
+sudo apt-get install -y python-software-properties python g++ make;
+sudo apt-get install -y software-properties-common;
+sudo add-apt-repository -y ppa:chris-lea/node.js;
+sudo apt-get update;
+sudo apt-get install -y nodejs;
+node --version;
 echo '############## INSTALL NPM ####################'
-npm config set tmp='/tmp'
-sudo chown -R $(whoami) $HOME/.npm
-sudo npm i -g npm
+npm config set tmp='/tmp';
+sudo chown -R $(whoami) $HOME/.npm;
+sudo npm i -g npm;
 echo '############## INSTALL MONGODB ####################'
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-apt-get update
-apt-get install mongodb-10gen
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10;
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list;
+sudo apt-get update;
+sudo apt-get install mongodb-10gen;
 set +o errexit #don't abort if any command fails
-mkdir /data/
-mkdir /data/db
-chmod 777 /data/db
+mkdir /data/;
+mkdir /data/db;
+chmod 777 /data/db;;
 mongod --version
 set -o errexit #abort if any command fails
-cd /media/sf_tp-p2018
-npm install --no-bin-links
-
+cd /media/sf_tp-p2018;
+npm install --no-bin-links;
 echo '############## INSTALL SERVER APP ####################'
-sudo npm install pm2 -g
-sudo chown -R $(whoami) $HOME/.npm
-pm2 start /media/sf_tp-p2018/app.js -name webschool
-pm2 startup ubuntu
-sudo env PATH=$PATH:/usr/local/bin pm2 startup ubuntu -u $(whoami)
-pm2 save
-
+sudo npm install pm2 -g;
+sudo chown -R $(whoami) $HOME/.npm;
+pm2 start /media/sf_tp-p2018/app.js -name webschool;
+pm2 startup ubuntu;
+sudo env PATH=$PATH:/usr/local/bin pm2 startup ubuntu -u $(whoami);
+pm2 save;
